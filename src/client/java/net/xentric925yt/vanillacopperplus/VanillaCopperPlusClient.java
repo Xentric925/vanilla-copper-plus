62668da0382c1be0722914207838ca3ab6161563
Dawoud Bayaa <davidbayaa2001@gmail.com>
2024-07-15 20:50:32 +0300

    First Commit

---
+++ src/client/java/net/xentric925yt/vanillacopperplus/VanillaCopperPlusClient.java
@@ -0,0 +1,10 @@
+package net.xentric925yt.vanillacopperplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaCopperPlusClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
